deb4369d728e1cc2f7bcf43acde31d2fb4b385ef
mikepreston17 <michael.n.preston@gmail.com>
2019-12-14 12:43:57 -0600

    ignoring .next

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,9 @@ import react from 'react'
 
 const Index = () => {
 
+    const data = await fetch('https://api.to.wtf/ping');
+    console.log(data);
+
     return (
         <div>
             <h1>Hello & Welcome to Upstack Connect!</h1>
