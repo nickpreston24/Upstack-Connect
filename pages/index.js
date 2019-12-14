ae1fcd9b2624a0acab4d65d180ef927dd94a249c
mikepreston17 <michael.n.preston@gmail.com>
2019-12-14 13:30:39 -0600

    test deploy

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ const Index = () => {
         <div>
             <h1>Hello & Welcome to Upstack Connect!</h1>
             <p>Some data:</p>
-            <i>`${msg}`</i>
+            <i>{`${msg}`}</i>
         </div>
     )
 }
