6351df86f55f1f859d61acec5308397a29c1ed85
mikepreston17 <michael.n.preston@gmail.com>
2019-12-14 12:39:14 -0600

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import react from 'react'
+
+const Index = () => {
+
+    return (
+        <div>
+            <h1>Hello & Welcome to Upstack Connect!</h1>
+        </div>
+    )
+}
+
+export default Index;
