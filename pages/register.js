b2d00ee8d478fa27d75e0bb9e9d4c6697f2b43a5
mikepreston17 <michael.n.preston@gmail.com>
2019-12-14 23:20:25 -0600

    Added registration page

---
+++ pages/register.js
@@ -0,0 +1,57 @@
+import Link from 'next/link';
+import axios from 'axios';
+
+const Profile = (props) => {
+
+    function signUp() {
+
+        const query = 'api/register'
+
+        const postData = {
+            "first_name": "Michael",
+            "last_name": "Preston",
+            phone: "+359884909301",
+            email: "michael.n.preston@gmail.com",
+            // password: "demoday",
+            password: "daef4953b9783365cad6615223720506cc46c5167cd16ab500fa597aa08ff964eb24fb19687f34d7665f778fcb6c5358fc0a5b81e1662cf90f73a2671c53f991",
+            address: "105 Bleecker Street",
+            city: "Brooklyn",
+            "pic_url": "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTw3dxAr-998XQnx4i8BeI2Gk3AUo2U-Wu7MO3nPTqIFIB2btxL",
+            location: {
+                lat: 44.4268,
+                lng: 25.10
+            },
+            paragraph: "Perferendis aut sit voluptatem consequatur accusantium. Accusantium sit perferendis consequatur voluptatem aut. Voluptatem accusantium consequatur sit aut perferendis.",
+            offers: "good company & much more",
+            country: "USA"
+        }
+
+        console.log(`Registering user ${postData["first_name"]}`)
+
+        axios.post(query, postData)
+            .then(response => console.log(response))
+            .catch(console.error)
+    }
+
+    return (
+        <div>
+            {/* Standins for fancier forms */}
+            <h2>Register Here</h2>
+            <label>UserName</label> <br />
+            <input></input><br />
+            <label>Password</label> <br />
+            <input></input><br />
+
+            <button type='submit' onClick={signUp}>Register</button><br />
+            <Link href="/">
+                <a>Go Home</a>
+            </Link>
+
+        </div>
+    )
+}
+
+
+export default Profile
+
+
